270d1c04411fa2d7893cd2d40b19747571a7e0d1
bonedaddy <alex@bonedaddy.io>
2024-12-30 02:30:43 -0800

    add docker compose file, operator script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # sophon_node_operator
+
+Tooling for running sophon nodes
+
+# Setup
+
+* Update `operator.sh` with your parameters
+
+
+# Starting
+
+```shell
+$> ./operator.sh start
+```
+
+# Logging
+
+```shell
+$> ./operator.sh logs
+```
---
+++ operator.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+export PORT=7007
+# example: 0xb33f
+export OPERATOR_ADDRESS=...
+# example: 0xb33f
+export DESTINATION_ADDRESS=...
+# example: 0.5
+export PERCENTAGE=...
+# example: http://127.0.0.1:7007
+export PUBLIC_DOMAIN=...
+
+# Function to show usage
+usage() {
+    echo "Usage: $0 [start|logs]"
+    echo "  start - Start the node"
+    echo "  logs  - Show node logs"
+    exit 1
+}
+
+# Check if an argument is provided
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+# Process the command argument
+case "$1" in
+    "start")
+        docker-compose up -d
+        ;;
+    "logs")
+        docker-compose logs
+        ;;
+    *)
+        usage
+        ;;
+esac
